76761886d9c65074fb678330e8cb8978241f731f
rigilz48 <rigilmakmun@gmail.com>
2024-12-13 05:39:31 +0700

    Update Alert

--- fe-indiafood/src/components/PopupMenu.jsx
+++ fe-indiafood/src/components/PopupMenu.jsx
@@ -60,23 +60,24 @@ const PopupMenu = ({
               >
                 Keranjang
               </button>
-              {showAlert && (
-                <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
-                  <div className='bg-white rounded-lg p-6 w-1/3 max-md:w-11/12'>
-                    <h3 className='text-2xl font-semibold'>
-                      Menu berhasil ditambahkan ke keranjang!
-                    </h3>
-                    <button
-                      onClick={closeAlert}
-                      className='bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full mt-4'
-                    >
-                      OK
-                    </button>
-                  </div>
-                </div>
-              )}
             </div>
           </div>
+
+          {showAlert && (
+            <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+              <div className='bg-white rounded-lg p-6 w-1/3 max-md:w-11/12'>
+                <h3 className='text-2xl font-semibold'>
+                  Menu berhasil ditambahkan ke keranjang!
+                </h3>
+                <button
+                  onClick={closeAlert}
+                  className='bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full mt-4'
+                >
+                  OK
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </>
