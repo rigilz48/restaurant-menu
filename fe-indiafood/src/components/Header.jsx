bf769aab1ebfeb2db1cd8350880985d4122e3ac1
rigilz48 <rigilmakmun@gmail.com>
2024-12-13 22:19:16 +0700

    Update Icon Cart

--- fe-indiafood/src/components/Header.jsx
+++ fe-indiafood/src/components/Header.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
+import { ShoppingCart } from '@phosphor-icons/react';
 import userLogo from '../assets/user/default.webp';
 
 function Header({ cart, toggleCart }) {
@@ -70,7 +71,7 @@ function Header({ cart, toggleCart }) {
 
             {/* Cart/Keranjang Icon */}
             <a href='#' className='relative' onClick={toggleCart}>
-              <svg
+              {/* <svg
                 className='w-6 h-6 text-gray-800 dark:text-white'
                 aria-hidden='true'
                 xmlns='http://www.w3.org/2000/svg'
@@ -86,7 +87,9 @@ function Header({ cart, toggleCart }) {
                   strokeWidth='2'
                   d='M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312'
                 />
-              </svg>
+              </svg> */}
+
+              <ShoppingCart size={24} />
 
               <span
                 href='/'
