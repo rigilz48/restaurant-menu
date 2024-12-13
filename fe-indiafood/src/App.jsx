ed37d50da86478d8014a98ccad2e075ee029045e
rigilz48 <rigilmakmun@gmail.com>
2024-12-13 23:58:29 +0700

    Update Dialog

--- fe-indiafood/src/App.jsx
+++ fe-indiafood/src/App.jsx
@@ -193,7 +193,7 @@ const App = () => {
                 <img
                   src='https://img.freepik.com/free-vector/kids-learning-cook-concept-illustration_114360-20995.jpg?t=st=1734108608~exp=1734112208~hmac=16e828a52eb7437b88d6f0c4709521622e4e1f683054205a8146ce37e6b4fbcc&w=1380'
                   alt='Preparing'
-                  className='w-80 h-60 mx-auto'
+                  className='w-96 h-64 mx-auto'
                 />
               </div>
             </div>
