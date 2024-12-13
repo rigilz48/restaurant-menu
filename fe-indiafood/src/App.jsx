5f22bc568beafec61ec1af1b4e7c6d11d1fb94af
rigilz48 <rigilmakmun@gmail.com>
2024-12-14 00:44:09 +0700

    Move Dialog to components

--- fe-indiafood/src/App.jsx
+++ fe-indiafood/src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import CardMenu from './components/CardMenu';
 import DrawerCart from './components/DrawerCart';
 import PopupMenu from './components/PopupMenu';
+import ConfirmDialog from './components/ConfirmDialog';
+import PreparingDialog from './components/PreparingDialog';
 
 const App = () => {
   const [dataMenus, setMenus] = useState([]);
@@ -160,44 +162,14 @@ const App = () => {
 
           {/* Confirm Dialog */}
           {showConfirmDialog && (
-            <div className='fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75 z-50'>
-              <div className='bg-white p-6 rounded-lg shadow-lg max-w-[90%] sm:max-w-[400px]'>
-                <h2 className='text-lg sm:text-xl font-semibold mb-4 text-center'>
-                  Yakin ingin memesan sekarang?
-                </h2>
-                <div className='flex justify-end sm:justify-end gap-4'>
-                  <button
-                    onClick={() => setShowConfirmDialog(false)}
-                    className='px-4 py-2 bg-gray-300 rounded-full w-full sm:w-auto'
-                  >
-                    Nanti dulu deh
-                  </button>
-                  <button
-                    onClick={handleConfirmOrder}
-                    className='px-4 py-2 bg-purple-600 text-white rounded-full w-full sm:w-auto'
-                  >
-                    Oke, pesan sekarang
-                  </button>
-                </div>
-              </div>
-            </div>
+            <ConfirmDialog
+              setShowConfirmDialog={setShowConfirmDialog}
+              handleConfirmOrder={handleConfirmOrder}
+            />
           )}
 
           {/* Preparing Dialog */}
-          {showPreparingDialog && (
-            <div className='fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75 z-50'>
-              <div className='bg-white p-6 rounded-lg shadow-lg text-center max-w-[90%] sm:max-w-[400px]'>
-                <h2 className='text-xl font-semibold mb-4'>
-                  Makanan kamu sedang disiapkan, ditunggu yaa!
-                </h2>
-                <img
-                  src='https://img.freepik.com/free-vector/kids-learning-cook-concept-illustration_114360-20995.jpg?t=st=1734108608~exp=1734112208~hmac=16e828a52eb7437b88d6f0c4709521622e4e1f683054205a8146ce37e6b4fbcc&w=1380'
-                  alt='Preparing'
-                  className='w-96 h-64 mx-auto max-w-full max-h-48 sm:h-auto sm:w-auto'
-                />
-              </div>
-            </div>
-          )}
+          {showPreparingDialog && <PreparingDialog />}
         </main>
         <Footer />
       </div>
---
+++ fe-indiafood/src/components/ConfirmDialog.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+const ConfirmDialog = ({ setShowConfirmDialog, handleConfirmOrder }) => {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75 z-50'>
+      <div className='bg-white p-6 rounded-lg shadow-lg max-w-[90%] sm:max-w-[400px]'>
+        <h2 className='text-lg sm:text-xl font-semibold mb-4 text-center'>
+          Yakin ingin memesan sekarang?
+        </h2>
+        <div className='flex justify-end sm:justify-end gap-4'>
+          <button
+            onClick={() => setShowConfirmDialog(false)}
+            className='px-4 py-2 bg-gray-300 rounded-full w-full sm:w-auto'
+          >
+            Nanti dulu deh
+          </button>
+          <button
+            onClick={handleConfirmOrder}
+            className='px-4 py-2 bg-purple-600 text-white rounded-full w-full sm:w-auto'
+          >
+            Oke, pesan sekarang
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmDialog;
