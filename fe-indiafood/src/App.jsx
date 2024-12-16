b56f40aa8c27fe297fdc490fc26a432c305f7126
rigilz48 <rigilmakmun@gmail.com>
2024-12-16 15:46:44 +0700

    Change loading,gif to SVG spinner

--- fe-indiafood/src/App.jsx
+++ fe-indiafood/src/App.jsx
@@ -175,11 +175,11 @@ const App = () => {
             {isError && (
               <div className='col-span-full flex justify-center items-center min-h-[50vh] text-red-500'>
                 <svg
-                  className='spinner'
+                  className='spinner text-purple-500'
                   xmlns='http://www.w3.org/2000/svg'
                   width='24'
                   height='24'
-                  fill='#9305ea'
+                  fill='none'
                   viewBox='0 0 256 256'
                 >
                   <path d='M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z'></path>
