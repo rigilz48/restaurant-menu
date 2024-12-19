75b07fa0ca2a96c95b5f88f5b460afb1d59bc21e
rigilz48 <rigilmakmun@gmail.com>
2024-12-19 18:05:53 +0700

    Improve foodmenuControllers

--- india-food-backend/controllers/foodmenuControllers.js
+++ india-food-backend/controllers/foodmenuControllers.js
@@ -1,20 +1,21 @@
 const pool = require('../config/connection');
 
-const getAllfoodmenu = async (request, response) => {
+const getAllFoodMenu = async (request, response) => {
   try {
     // Ambil query parameter untuk pagination
-    const page = parseInt(request.query.page) || 1; // Halaman default 1
-    const limit = parseInt(request.query.limit) || 8; // Limit default 8 data per halaman
+    const page = parseInt(request.query.page, 10) || 1; // Halaman default 1
+    const limit = parseInt(request.query.limit, 10) || 8; // Limit default 8 data per halaman
     const offset = (page - 1) * limit; // Hitung offset berdasarkan halaman
 
+    // SQL queries
+    const GET_MENUS_QUERY = `SELECT * FROM menu_makanan ORDER BY id_makanan LIMIT $1 OFFSET $2`;
+    const COUNT_MENUS_QUERY = `SELECT COUNT(*) FROM menu_makanan`;
+
     // Query untuk mengambil data sesuai limit dan offset
-    const result = await pool.query(
-      `SELECT * FROM menu_makanan ORDER BY id_makanan LIMIT $1 OFFSET $2`,
-      [limit, offset]
-    );
+    const result = await pool.query(GET_MENUS_QUERY, [limit, offset]);
 
     // Query untuk menghitung total data di tabel
-    const totalResult = await pool.query(`SELECT COUNT(*) FROM menu_makanan`);
+    const totalResult = await pool.query(COUNT_MENUS_QUERY);
     const totalMenus = parseInt(totalResult.rows[0].count, 10);
     const totalPages = Math.ceil(totalMenus / limit);
 
@@ -31,14 +32,15 @@ const getAllfoodmenu = async (request, response) => {
   }
 };
 
-const getSlugfoodmenu = async (request, response) => {
+const getSlugFoodMenu = async (request, response) => {
   try {
-    const result = await pool.query(
-      `SELECT * FROM menu_makanan WHERE slug = $1`,
-      [request.params.slug]
-    );
+    // SQL queries
+    const GET_MENU_BY_SLUG_QUERY = `SELECT * FROM menu_makanan WHERE slug = $1`;
 
-    let dataResult = result.rows[0];
+    const result = await pool.query(GET_MENU_BY_SLUG_QUERY, [
+      request.params.slug,
+    ]);
+    const dataResult = result.rows[0];
 
     if (!dataResult) {
       response.status(404).json({ message: 'Data Not Found' });
@@ -51,4 +53,4 @@ const getSlugfoodmenu = async (request, response) => {
   }
 };
 
-module.exports = { getAllfoodmenu, getSlugfoodmenu };
+module.exports = { getAllFoodMenu, getSlugFoodMenu };
