4f4d79adc1335460dc3f4d4aa0712a3456d4ff6d
Rigil Makmun <44825015+rigilz48@users.noreply.github.com>
2025-08-16 16:04:44 +0700

    Create api/index.js
    API Vercel

---
+++ india-food-backend/api/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const foodmenuRoutes = require("../routes/foodmenuRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", foodmenuRoutes);
+
+// ❌ Jangan pakai app.listen di Vercel
+// ✅ Export handler
+module.exports = app;
