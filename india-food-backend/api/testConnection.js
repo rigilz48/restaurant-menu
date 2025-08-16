6b79d6f173b473d3b5279375999eea2d8482f676
Rigil Makmun <44825015+rigilz48@users.noreply.github.com>
2025-08-16 16:17:18 +0700

    Create api/testConnection.js
    Add tes API Vercel

---
+++ india-food-backend/api/testConnection.js
@@ -0,0 +1,19 @@
+// api/testConnection.js
+const pool = require('../config/connection'); // sesuaikan path
+
+export default async function handler(req, res) {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.status(200).json({
+      success: true,
+      message: 'PostgreSQL Connected',
+      serverTime: result.rows[0].now,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Connection failed',
+      error: error.message,
+    });
+  }
+}
