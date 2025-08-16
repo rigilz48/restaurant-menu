c3176a9c934582e27ea1f95baca7c456a37bd3e2
Rigil Makmun <44825015+rigilz48@users.noreply.github.com>
2025-08-16 16:22:42 +0700

    Create connectionver.js
    Connection Vercel

---
+++ india-food-backend/config/connectionver.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 20,
+  idleTimeoutMillis: 2000,
+  connectionTimeoutMillis: 2000,
+});
+
+module.exports = pool;
